66054c90a85dbf63939aae926b3865c5080ffa81
smallbee <smallbee3@gmail.com>
2019-01-03 19:06:28 +0900

    routing| Show loading message in Card.vue until fetching data from the server finished

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
       Card
-      <div>cid: {{cid}}</div>
+      <div v-if="loading">loading card...</div>
+      <div v-else>
+        <div>cid: {{cid}}</div>
+      </div>
     </div>
 </template>
 
@@ -9,17 +12,32 @@
 export default {
   data() {
     return {
-      cid: 0
+      cid: 0,
+      loading: false
     }
   },
   watch: {
     // '$route'(newVal, oldVal) {   // '$route' works!
     $route() {
-      this.cid = this.$route.params.cid
+      // this.cid = this.$route.params.cid
+      // cid가 변경될 때마다 백엔드 api를 호출하는 로직.
+      // 그래서 화면을 갱신하는 것이기 때문에 여기에다가
+      // fetchData를 호출하는게 맞아요.
+      this.fetchData()
     }
   },
   created() {
-    this.cid = this.$route.params.cid
+    this.fetchData()
+    // this.cid = this.$route.params.cid
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      setTimeout(() => {
+        this.cid = this.$route.params.cid
+        this.loading = false
+      }, 500)
+    }
   }
 }
 </script>
