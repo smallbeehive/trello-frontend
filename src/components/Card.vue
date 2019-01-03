df53ebf3a67bf57288847dbf8d8e34048be05814
smallbee <smallbee3@gmail.com>
2019-01-03 19:15:39 +0900

    routing| Code of loading message refactored by using 'watch' attribute's hanlder and immediate options

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -16,19 +16,35 @@ export default {
       loading: false
     }
   },
+  // watch: {
+  //   // '$route'(newVal, oldVal) {   // '$route' works!
+  //   $route() {
+  //     // this.cid = this.$route.params.cid
+  //     // cid가 변경될 때마다 백엔드 api를 호출하는 로직.
+  //     // 그래서 화면을 갱신하는 것이기 때문에 여기에다가
+  //     // fetchData를 호출하는게 맞아요.
+  //     this.fetchData()
+  //   }
+  // },
+  // created() {
+  //   this.fetchData()
+  //   // this.cid = this.$route.params.cid
+  // },
   watch: {
-    // '$route'(newVal, oldVal) {   // '$route' works!
-    $route() {
-      // this.cid = this.$route.params.cid
-      // cid가 변경될 때마다 백엔드 api를 호출하는 로직.
-      // 그래서 화면을 갱신하는 것이기 때문에 여기에다가
-      // fetchData를 호출하는게 맞아요.
-      this.fetchData()
+    // 좀 더 코드를 개선해볼게요. watch를 설정할 때
+    // 이렇게도 할 수 있어요.
+    // 함수가 아닌 객체를 설정할 수 있는데
+    // handler를 fetchData라는 문자열로 주고요
+    // 그렇게 되면 이 fetchData를 실행하는 거예요.
+    // 그리고 immediate라는 boolean값이 있는데
+    // true를 주면 바로 즉시 시행하는 거예요.
+    // created 안의 훅과 중복이죠.
+
+    // $route: {
+    '$route': {
+      handler: 'fetchData',
+      immediate: true
     }
-  },
-  created() {
-    this.fetchData()
-    // this.cid = this.$route.params.cid
   },
   methods: {
     fetchData() {
