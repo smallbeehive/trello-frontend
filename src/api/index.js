dbc1fc12ff4e543d1eff33f7631c2161eedf4c75
smallbee <smallbee3@gmail.com>
2019-01-04 13:01:27 +0900

    auth| export 'auth' object which has login function

--- src/api/index.js
+++ src/api/index.js
@@ -5,11 +5,12 @@ const DOMAIN = 'http://localhost:3000'
 const UNAUTHORIZED = 401
 const onUnauthorized = () => {
   // this.$router.replace('/login')
-  router.push('/login')
 
-  // router.push(`/login?rPath=${encodeURIComponent(location.pathname)}`)
+  // router.push('/login')
+  router.push(`/login?rPath=${encodeURIComponent(location.pathname)}`)
   // -> src/router/index.js에서 네비게이션 가드를 통해 모든 라우터에 적용해서
   //    위 코드는 필요 없음.
+  // -> 이 아닌듯..
 }
 
 const request = (method, url, data) => {
@@ -22,14 +23,23 @@ const request = (method, url, data) => {
       // const {'status' : status} = result.response
       const {status} = result.response
       if (status === UNAUTHORIZED) return onUnauthorized()
+
       // 처리 되지 않는 401 이외의 에러들은 throw
       // 나중에 필요하면 추가로 로직을 개발하도록 함.
-      throw Error(result)
+      // throw Error(result)
+      throw result.response
     })
 }
 
 export const board = {
   fetch() {
     return request('get', '/boards')
+  }
+}
+
+export const auth = {
+  login(email, password) {
+    // return request('post', '/login', {'email': email, 'password': password})
+    return request('post', '/login', {email, password})
   }
 }
