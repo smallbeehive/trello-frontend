5281a1f78842a8e9a4ffdc7c6703aa54e6ca1e42
smallbee <smallbee3@gmail.com>
2019-01-04 13:49:40 +0900

    auth| cherry-pick 2af7d03 - Navbar.vue template and style updated

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,15 @@
+<!-- 네비게이션 역할을 하는 부분은 Navbar라는 컴포넌트로
+분리해 낼거예요. -->
 <template>
-  <div>
-    <!-- 네비게이션 역할을 하는 부분은 Navbar라는 컴포넌트로
-    분리해 낼거예요. -->
-    <router-link to="/">Home</router-link>
-    <router-link to="/login">Login</router-link>
-  </div>
+  <nav class="header">
+    <div class="header-logo">
+      <router-link to="/">Home</router-link>
+    </div>
+    <div class="header-auth">
+      <router-link to="/login">Login</router-link>
+      <!-- <a>Logout</a> -->
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -14,4 +19,46 @@ export default {
 </script>
 
 <style>
+.header {
+  flex: none;
+  background-color: rgba(0,0,0,.15);
+  height: 32px;
+  padding: 4px;
+}
+.header a {
+  display: block;
+  height: 30px;
+  line-height: 30px;
+  text-decoration: none;
+  color: rgba(255,255,255,.5);
+}
+.header-logo {
+  position: absolute;
+  left: 50%;
+  top: 7px;
+  margin-left: -30px;
+  text-align: center;
+  font-weight: bolder;
+  font-size: 24px;
+}
+.header-logo a:hover,
+.header-logo a:focus {
+  color: rgba(255,255,255,.9);
+}
+.header-auth {
+  position: absolute;
+  right: 15px;
+  top: 5px;
+}
+.header-auth a {
+  border-radius: 2px;
+  padding: 0 10px;
+  background-color: rgba(255,255,255, .5);
+  color: white;
+  transition: all .3s;
+}
+.header-auth a:hover,
+.header-auth a:focus {
+  background-color: rgba(255,255,255, .3);
+}
 </style>
