0d372fd6d55321e15db3d5c1c7f56289f9a54ca2
smallbee <smallbee3@gmail.com>
2019-01-04 13:33:56 +0900

    auth| Login - (2) Get repath by 'this.$route.query' and redirect to repath when login succeed

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -28,6 +28,7 @@ export default {
       email: '',
       password: '',
       error: '',
+      rPath: ''
     }
   },
   computed: {
@@ -35,13 +36,23 @@ export default {
       return !this.email || !this.password
     }
   },
+  created() {
+    // 이제 리턴 경로로, 즉 요 rPath라는 곳으로 리다이렉트 할 필요가 있어요.
+    // 리다이렉트 같은 경우에는 url을 통해서 가져올 수 있어요.
+    // 그래서 저는 컴포넌트가 생성될 때 created죠.
+    // 컴포넌트가 생성될 때 rPath라는 곳에다가 저장을 할거예요.
+    // 라우트 정보는 $route로 접속할 수 있구요 쿼리 문자열은 query로 접속할 수 있어요.
+    // rPath를 가져옵니다. 아무것도 없을 경우에는 기본적으로 메인 경로로 이동하도록 했습니다.
+    this.rPath = this.$route.query.rPath || '/'
+  },
   methods: {
     onSubmit() {
       // console.log(this.email, this.password)
       auth.login(this.email, this.password)
         .then(data => {
+          // console.log(data)
           localStorage.setItem('token', data.accessToken)
-          console.log(data)
+          this.$router.push(this.rPath)
         })
         .catch(err => {
           console.log(err)
