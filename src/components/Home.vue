37f3e70987bf68e3b4974af25a60122a6d8129ad
smallbee <smallbee3@gmail.com>
2019-01-04 12:24:14 +0900

    auth| template fixed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,19 +4,19 @@
       <div>
         Board List:
         <div v-if="loading">Loading...</div>
-        <div v-else>Api result:
+        <div v-else>
           <div v-for="b in boards" :key="b.id">
-            {{b}}
+            <pre>{{b}}</pre>
           </div>
         </div>
-        <ul>
+        <!-- <ul>
           <li>
             <router-link to="/b/1">Board 1</router-link>
           </li>
           <li>
             <router-link to="/b/2">Board 2</router-link>
           </li>
-        </ul>
+        </ul> -->
       </div>
     </div>
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <!-- 네비게이션 역할을 하는 부분은 Navbar라는 컴포넌트로
     분리해 낼거예요. -->
     <router-link to="/">Home</router-link>
