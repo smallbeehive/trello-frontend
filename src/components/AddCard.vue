4399d9c1a4e1675975047a56fdf13b079a652087
smallbee <smallbee3@gmail.com>
2019-01-05 22:24:10 +0900

    card| Add card API - (2) 'ADD_CARD' action called in AddCard.vue and new card objects made after the api call

--- src/components/AddCard.vue
+++ src/components/AddCard.vue
@@ -10,11 +10,13 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex'
+
 export default {
+  props: ['listId'],
   data() {
     return {
       inputTitle: '',
-      valid: ''
     }
   },
   // watch: {
@@ -40,13 +42,29 @@ export default {
     // 닫아달라고 요청하면 되겠죠.
   },
   methods: {
+    ...mapActions([
+      'ADD_CARD'
+    ]),
     onSubmit() {
-      console.log('submit')
+      if (this.invalidInput) return
+      // 호출하기 전에 invalidInput이면 반환하도록 방어코드를 만듭니다.
+      // 그리고 나서 action에 전달하기 위한 인자들을 가져오는데요
+      // this에서 inputTitle을 가져옵니다. 입력한 값이죠.
+      // 그리고 우리 ADD_CARD action을 호출할 때는 title 뿐만이 아니라
+      // listId와 postion 정보도 가져오게 되있어요.
+      // 제가 api를 만들때 postion 값은 optional 값으로 정했기 때문에 전달해주지 않아도 됩니다.
+      // 대신에 listId는 반드시 전달해줘야되요.
+      // 그래서 AddCard에서 listId도 받아와야 되는데 우리는 요거를
+      // 부모 컴포넌트 List component 에서 받도록 할게요.
+
+      const {inputTitle, listId} = this
+      this.ADD_CARD({title: inputTitle, listId})
+        .finally(() => this.inputTitle = '')
     },
     setupClickOutside(el) {
       document.querySelector('body').addEventListener('click', e => {
-        // if (el.contains(e.target)) return
-        if (el.contains(e.currentTarget)) return
+        // if (el.contains(e.currentTarget)) return
+        if (el.contains(e.target)) return
         this.$emit('close')
       })
     }
