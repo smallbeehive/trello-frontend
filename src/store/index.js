970a17efee4b0a5d77a7e4d37ab3ce7354bfee07
smallbee <smallbee3@gmail.com>
2019-01-05 11:45:31 +0900

    vuex| action - actions defined in vuex store and api functions imported as '*(asterisk)'

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as api from '../api'
+// 아 요부분이네요. 요렇게 해야죠.
+// 왜냐면 api 부분에보면 board와 auth를 반환하고 있어요.
+// 요렇게 api라는 닉네임으로 받아줘야 겠죠.
 
 Vue.use(Vuex)
 
@@ -10,6 +14,14 @@ const store = new Vuex.Store({
   mutations: {
     SET_IS_ADD_BOARD (state, toggle) {
       state.isAddBoard = toggle
+    }
+  },
+  actions: {
+    // context 객체를 처음에 받는데 저는 사용하지 않을거니까
+    // _로 해놓고요. 입력은 어떤 객체로 받을건데
+    // title이라는 키를 사용하겠습니다.
+    ADD_BOARD (_, {title}) {
+      return api.board.create(title)
     }
   }
 })
