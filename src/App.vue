45ade828e8f94c5ee72bcc20912ead69e248bb92
smallbee <smallbee3@gmail.com>
2019-01-05 19:17:48 +0900

    board| cherry-pick ef62a1b - Board.vue template and style updated and Board title showed in the board detail pageg

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Navbar />
-    <router-view></router-view>
+    <router-view class="container"></router-view>
     <!-- router-view를 사용하게 되면 router-view를 선안한 위 아래
     header를 놓거나 footer를 놔서 앱에서 항상 보여할 부분들을
     App.vue에 선언하고 routing 경로에 따라 바꿔야 할 부분은
--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <!-- <div>
       Board
       <div v-if="loading">loading board...</div>
       <div v-else>
@@ -10,6 +10,15 @@
       </div>
       <hr />
       <router-view></router-view>
+    </div> -->
+    <div>
+      <div class="board-wrapper">
+        <div class="board">
+          <div class="board-header">
+            <span class="board-title">{{board.title}}</span>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
@@ -58,4 +67,78 @@ export default {
 </script>
 
 <style>
+.board-wrapper {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+}
+.board {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.board-header {
+  flex: none;
+  padding: 8px 4px 8px 8px;
+  margin: 0;
+  height: 32px;
+  line-height: 32px;
+}
+.board-header input[type=text] {
+  width: 200px;
+}
+.board-header-btn {
+  border-radius: 4px;
+  padding: 2px 10px;
+  height: 30px;
+  margin-bottom: 15px;
+  display: inline-block;
+  color: #fff;
+}
+.board-header-btn:hover,
+.board-header-btn:focus {
+  background-color: rgba(0,0,0,.15);
+  cursor: pointer;
+}
+.board-title {
+  font-weight: 700;
+  font-size: 18px;
+}
+.show-menu {
+  font-size: 14px;
+  position: absolute;
+  right: 15px;
+}
+.list-section-wrapper {
+  flex-grow: 1;
+  position: relative;
+}
+.list-section {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow-x: auto;
+  overflow-y: hidden;
+  white-space: nowrap;
+  padding: 0 10px;
+}
+.list-wrapper {
+  display: inline-block;
+  height: 100%;
+  width: 272px;
+  vertical-align: top;
+  margin-right: 5px;
+}
+.card-item.gu-transit {
+  background-color: #555 !important;
+}
+.card-item.gu-mirror {
+  opacity: 1 !important;
+  background-color: #fff !important;
+  transform: rotate(3deg) !important;
+}
 </style>
