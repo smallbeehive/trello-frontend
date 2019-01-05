2aca0e33c08ade64a188277c59f53324d4bb425d
smallbee <smallbee3@gmail.com>
2019-01-05 20:46:14 +0900

    card| Catch the event when clicking the outside of current component element and call 'this.$emit('close')' to close the input element

--- src/components/AddCard.vue
+++ src/components/AddCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="add-card">
-    <form>
+    <form @submit.prevent="onSubmit">
       <input class="form-control" type="text" v-model="inputTitle" ref="inputText">
       <!-- <button class="btn" :class="{'btn-success': valid}" type="submit" >Add Card</button> -->
       <button class="btn btn-success" :disabled="invalidInput" type="submit" >Add Card</button>
@@ -30,6 +30,26 @@ export default {
   },
   mounted() {
     this.$refs.inputText.focus()
+    this.setupClickOutside(this.$el)
+    // this.$el하면 이 AddCard component의 element가 넘어가요.
+    // 그걸 setupClickOutside에서 el로 받겠습니다.
+    // 그래서 el에 DOM 함수중에 contains 함수가 있어요.
+    // 이게 클릭한 element를 포함하고 있는지, 포함하고 있다면 현재 컴포넌트를
+    // 클릭한 거기 때문에 아무 동작도 하지 않구요.
+    // 그렇지 않을 경우에만 close event를 emit해서 List.vue component한테
+    // 닫아달라고 요청하면 되겠죠.
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit')
+    },
+    setupClickOutside(el) {
+      document.querySelector('body').addEventListener('click', e => {
+        // if (el.contains(e.target)) return
+        if (el.contains(e.currentTarget)) return
+        this.$emit('close')
+      })
+    }
   }
 }
 </script>
