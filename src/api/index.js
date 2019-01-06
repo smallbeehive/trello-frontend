0f0abb5a7bc6a4ad7e3c339d04d1a111e3d0edea
smallbee <smallbee3@gmail.com>
2019-01-06 20:33:25 +0900

    list| Delete list API - (1) 'destroy' method added to list object in api/index.js and 'DELETE_LIST' action added

--- src/api/index.js
+++ src/api/index.js
@@ -82,6 +82,9 @@ export const list = {
   },
   update(id, payload) {
     return request('put', `/lists/${id}`, payload)
+  },
+  destroy(id) {
+    return request('delete', `/lists/${id}`)
   }
 }
 
--- src/store/actions.js
+++ src/store/actions.js
@@ -43,6 +43,10 @@ const actions = {
     return api.list.update(id, {pos, title})
       .then(_ => dispatch('FETCH_BOARD', {id: state.board.id}))
   },
+  DELETE_LIST ({dispatch, state}, {id}) {
+    return api.list.destroy(id)
+      .then(_ => dispatch('FETCH_BOARD', {id: state.board.id}))
+  },
 
   // ADD_CARD (context, {title, listId, pos) {
   ADD_CARD ({dispatch, state}, {title, listId, pos}) {
