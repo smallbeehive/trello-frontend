7de1085b7b972bf8b5978d5af1624d219b2d609a
smallbee <smallbee3@gmail.com>
2019-01-06 12:40:13 +0900

    card| Color theme - (3) navbarColor and bodyColor applied to elements in updateTheme method of Navbar.vue using vuex state

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,7 @@
 <script>
 // import {setAuthInHeader} from '../api'
 // 여기서 더이상 api 호출은 하지 않으니까 삭제하구요.
-import {mapGetters, mapMutations} from 'vuex'
+import {mapState, mapGetters, mapMutations} from 'vuex'
 
 export default {
   computed: {
@@ -23,7 +23,14 @@ export default {
     // }
     ...mapGetters([
       'isAuth'
-    ])
+    ]),
+    ...mapState({
+      navbarColor: 'navbarColor',
+      bodyColor: 'bodyColor'
+    })
+  },
+  mounted() {
+    this.updateTheme()
   },
   methods: {
     ...mapMutations([
@@ -34,6 +41,27 @@ export default {
       // setAuthInHeader(null)
       this.LOGOUT()
       this.$router.push('/login')
+    },
+    // (Homoe.vue의 created()에서 this.SET_THEME()을 호출해도 변화가 없음.)
+    // 왜냐하면 이 Home component 에서는 단순히 SET_THEME라는 mutation을 호출했고,
+    // mutation에서 상태값만 저장했기 때문이에요.
+    // 그러면 이 상태값이 변할 때 요 상태값을 참조해서 색상을 변화시켜 줘야겠죠.
+    // 고거 같은 경우에는 요 Navbar component가 있어요.
+    // Navbar component는 보드에 들어가거나 홈에 나오거나 둘다 사용하고 있는
+    // component이기 때문에 여기에다가 테마 로직을 추가하면 되겠습니다.
+    updateTheme() {
+      // 이 updateTheme에서 색상을 변경하려면 관련된 element들을 가져와야 겠죠.
+      // 먼저 이 Navbar부터 색상을 변경해보도록 하겠습니다.
+
+      // 1) Navbar
+      // this의 $el하면 지금 navbar element를 가져온거예요.
+      this.$el.style.backgroundColor = this.navbarColor
+
+      // 2) Body
+      // 그리고 또 남은것이 아래 부분을 또 세팅해줘야 하는데
+      // 고거는 함 보죠. 요 컨테이너(.containe) 부분이에요.
+      const container = document.querySelector('.container')
+      if (container) container.style.backgroundColor = this.bodyColor
     }
   }
 }
