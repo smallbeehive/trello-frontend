689fe3e62b1ea3b4255c1a60f9d9f6ccdf5a8b96
smallbee <smallbee3@gmail.com>
2019-01-06 16:38:24 +0900

    list| Add list API - (3) Show and hide input element using 'isAddList' data toggle and focus event applied to the input element using 'this.$nextTick()'

--- src/components/AddList.vue
+++ src/components/AddList.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="add-list">
-    <input type="text" class='form-control'>
+    <input v-if="isAddList" type="text" class="form-control"
+          v-model="inputTitle"
+          ref="inputTitle">
+    <a v-else href="" @click.prevent="onAddList">&plus; Add another list</a>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      isAddList: false,
+      inputTitle: ''
+    }
+  },
+  methods: {
+    onAddList() {
+      this.isAddList = true
+      // this.$refs.inputTitle.focus()
+      // [ 리스트 생성 ]
+      // 요거는 렌더링 사이클에 의해 다음 틱에서 돌아야 하기 때문에
+      // nextTick 함수 안에 넣어줍니다.
+      this.$nextTick(_ => this.$refs.inputTitle.focus())
+    }
+  }
 }
 </script>
 
